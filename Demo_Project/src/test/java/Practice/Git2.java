1de1b7d0d18f75c89c950b66caac5d377201e439
kumarprince1995 <kumarprincesailor@rediffmail.com>
2021-08-30 20:04:17 +0530

    Commiting

---
+++ Demo_Project/src/test/java/Practice/Git2.java
@@ -0,0 +1,25 @@
+package Practice;
+
+
+import org.testng.annotations.Test;
+
+public class Git2 {
+	
+	@Test
+	public void createOrgTest() {
+		System.out.println("execute createOrg Test");
+ 	}	
+	
+	
+	@Test
+	public void createOrgWithIndustriesTest() {
+		System.out.println("execute createOrgWithIndustries Test");
+ 	}
+	
+	
+	@Test
+	public void createOrgTestWithRatingTest() {
+		System.out.println("execute createOrgTestWithRating Test");
+ 	}
+
+}
